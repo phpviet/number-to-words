6c22c05823d7ad6a2836f753b60e5bc2b11a7bdb
Vuong Minh <vuongxuongminh@gmail.com>
2019-06-02 10:08:00 +0700

    Added dictionary

---
+++ src/Dictionary.php
@@ -0,0 +1,186 @@
+<?php
+/**
+ * @link https://github.com/phpviet/number-to-words
+ * @copyright (c) PHP Viet
+ * @license [MIT](http://www.opensource.org/licenses/MIT)
+ */
+
+namespace PHPViet\NumberToWords;
+
+use InvalidArgumentException;
+
+/**
+ * @author Vuong Minh <vuongxuongminh@gmail.com>
+ * @since 1.0.0
+ */
+class Dictionary implements DictionaryInterface
+{
+    /**
+     * Mảng tập hợp hàng đơn vị.
+     *
+     * @var array
+     */
+    protected static $tripletUnits = [
+        '',
+        'một',
+        'hai',
+        'ba',
+        'bốn',
+        'năm',
+        'sáu',
+        'bảy',
+        'tám',
+        'chín'
+    ];
+
+    /**
+     * Mảng tập hợp hàng chục.
+     *
+     * @var array
+     */
+    protected static $tripletTens = [
+        '',
+        'mười',
+        'hai mươi',
+        'ba mươi',
+        'bốn mươi',
+        'năm mươi',
+        'sáu mươi',
+        'bảy mươi',
+        'tám mươi',
+        'chín mươi'
+    ];
+
+    /**
+     * Từ ngữ mô tả hàng trăm.
+     *
+     * @var string
+     */
+    protected static $hundred = 'trăm';
+
+    /**
+     * Mảng tập hợp đơn vị tính dựa trên số mũ 3.
+     *
+     * @var array
+     */
+    protected static $exponents = [
+        '',
+        'nghìn',
+        'triệu',
+        'tỷ',
+        'nghìn tỷ',
+        'triệu tỷ'
+    ];
+
+    /**
+     * @inheritDoc
+     */
+    public function zero(): string
+    {
+        return 'không';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function minus(): string
+    {
+        return 'âm';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function separator(): string
+    {
+        return ' ';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function tripletTenSeparator(): string
+    {
+        return 'linh';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function specialTripletUnitOne(): string
+    {
+        return 'mốt';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function specialTripletUnitFour(): string
+    {
+        return 'tư';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function specialTripletUnitFive(): string
+    {
+        return 'lăm';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function fraction(): string
+    {
+        return 'phẩy';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getTripletUnit(int $unit): string
+    {
+        if (!isset(static::$tripletUnits[$unit])) {
+            throw new InvalidArgumentException('`ten` arg must be in 0-9 range!');
+        }
+
+        return static::$tripletUnits[$unit];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getTripletTen(int $ten): string
+    {
+        if (!isset(static::$tripletTens[$ten])) {
+            throw new InvalidArgumentException('`ten` arg must be in 0-9 range!');
+        }
+
+        return static::$tripletTens[$ten];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getTripletHundred(int $hundred): string
+    {
+        if (!isset(static::$tripletUnits[$hundred])) {
+            throw new InvalidArgumentException('`hundred` arg must be in 0-9 range!');
+        }
+
+        return static::$tripletUnits[$hundred] . $this->separator() . static::$hundred;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getExponent(int $power): string
+    {
+        if (!isset(static::$exponents[$power])) {
+            throw new InvalidArgumentException('`power` arg not exist in vietnamese dictionary!');
+        }
+
+        return static::$exponents[$power];
+    }
+}
---
+++ src/DictionaryInterface.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * @link https://github.com/phpviet/number-to-words
+ * @copyright (c) PHP Viet
+ * @license [MIT](http://www.opensource.org/licenses/MIT)
+ */
+
+namespace PHPViet\NumberToWords;
+
+/**
+ * @author Vuong Minh <vuongxuongminh@gmail.com>
+ * @since 1.0.0
+ */
+interface DictionaryInterface
+{
+    /**
+     * Trả về từ ngữ diễn giải số 0.
+     *
+     * @return string
+     */
+    public function zero(): string;
+
+    /**
+     * Trả về từ ngữ diễn giải số âm.
+     *
+     * @return string
+     */
+    public function minus(): string;
+
+    /**
+     * Trả về giá trị ngắt các chữ số.
+     *
+     * @return string
+     */
+    public function separator(): string;
+
+    /**
+     * Trả về từ ngữ ngắt giữa nguyên số và phân số.
+     *
+     * @return string
+     */
+    public function fraction(): string;
+
+    /**
+     * Trả về từ ngữ khi chuyển đổi 1 sang chữ số khi chữ số hàng chục lớn hơn 1.
+     *
+     * @return string
+     */
+    public function specialTripletUnitOne(): string;
+
+    /**
+     * Trả về từ ngữ khi chuyển đổi 4 sang chữ số khi chữ số hàng chục lớn hơn hoặc bằng 2.
+     *
+     * @return string
+     */
+    public function specialTripletUnitFour(): string;
+
+    /**
+     * Trả về từ ngữ khi chuyển đổi 5 sang chữ số khi chữ số hàng chục lớn hơn hoặc bằng 1.
+     *
+     * @return string
+     */
+    public function specialTripletUnitFive(): string;
+
+    /**
+     * Trả về từ ngữ nằm giữa hàng trăm và hàng đơn vị khi chữ số hàng chục là 0 và chữ số hàng trăm, đơn vị khác 0.
+     *
+     * @return string
+     */
+    public function tripletTenSeparator(): string;
+
+    /**
+     * Trả về từ ngữ hàng đơn vị tương ứng với số truyền vào.
+     *
+     * @param int $unit hàng đơn vị
+     * @return string
+     */
+    public function getTripletUnit(int $unit): string;
+
+    /**
+     * Trả về từ ngữ hàng chục tương ứng với số truyền vào.
+     *
+     * @param int $ten hàng chục
+     * @return string
+     */
+    public function getTripletTen(int $ten): string;
+
+    /**
+     * Trả về từ ngữ hàng trăm tương ứng với số truyền vào.
+     *
+     * @param int $hundred hàng trăm
+     * @return string
+     */
+    public function getTripletHundred(int $hundred): string;
+
+    /**
+     * Trả về đơn vị tương ứng với số mũ.
+     *
+     * @param int $power hàng trăm
+     * @return string
+     */
+    public function getExponent(int $power): string;
+
+}
